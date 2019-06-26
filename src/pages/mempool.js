b9d1e015d65570c0a5669773ed694c69fe688a80
Bill Lou <billloukehao@gmail.com>
2019-06-26 15:29:23 -0700

    Edit CSS for Mempool page

--- src/pages/mempool.js
+++ src/pages/mempool.js
@@ -14,15 +14,27 @@ const Mempool = () => (
         <h3>Secret Key confirmed! Your transaction has been sent to the Mempool</h3>
       </div>
       <h1>What is the Mempool?</h1>
-      <img src="https://blockbyblock.s3.us-east-2.amazonaws.com/mempool.svg" alt="mempool" />
       <div className="mempool_description">
-        <p>The Mempool is the place where unprocessed transactions wait to
-            be filled. Every interval (around 10 mins), the Bitcoin network picks
-            a group of transactions from the Mempool and executes them all in
-            a batch called a ‘block’. Since each block has a set size of 1MB, not
+        <div className="mempool_image">
+          <img src="https://blockbyblock.s3.us-east-2.amazonaws.com/mempool.svg" alt="mempool" />
+          <div className="mempool_image_label">
+            <img src="https://blockbyblock.s3.us-east-2.amazonaws.com/red_box.svg" alt="red box" />
+            <p>Our transaction</p>
+          </div>
+          <div className="mempool_image_label">
+            <img src="https://blockbyblock.s3.us-east-2.amazonaws.com/green_box.svg" alt="green box" />
+            <p>Other transactions</p>
+          </div>
+        </div>
+        <div className="mempool_text">
+          <p>The Mempool is the place where unprocessed transactions wait to
+              be filled. Every interval (around 10 mins), the Bitcoin network picks
+              a group of transactions from the Mempool and executes them all in
+              a batch called a ‘block’. Since each block has a set size of 1MB, not
             all transactions can fit inside a single block.</p>
-        <p>This can cause a buildup in the Mempool if too many transactions
+          <p>This can cause a buildup in the Mempool if too many transactions
             are being processed through-out the network.</p>
+        </div>
       </div>
     </div>
     <ProgressBar location={3} pointerDes="Mempool" />
